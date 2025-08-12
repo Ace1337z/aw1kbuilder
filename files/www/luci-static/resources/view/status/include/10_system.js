8891581db7a8f8ebee1e569416c78e4a68a3228a
Ace1337z <hasbisyahman@pm.me>
2025-08-13 04:17:09 +0800

    Update 10_system.js

--- files/www/luci-static/resources/view/status/include/10_system.js
+++ files/www/luci-static/resources/view/status/include/10_system.js
@@ -43,7 +43,7 @@ return baseclass.extend({
 		]);
 
 		const fields = [
-			[_('Firmware'), 'AW1K NIALWRT'],
+			[_('Firmware'), 'SPADES V1.0'],
 			[_('Version'), versionString],
 			[_('Time'), datestr],
 			[_('Uptime'), info.uptime ? '%t'.format(info.uptime) : '?'],
